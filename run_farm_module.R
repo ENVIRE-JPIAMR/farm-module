e04d9a59097fb0fd77b7d4f823b790ac1014fbda
Subhasishbasak <subhasish.basak@anses.fr>
2024-02-08 19:01:56 +0100

    new outputs Becker et al. (2022)

--- run_farm_module.R
+++ run_farm_module.R
@@ -3,12 +3,21 @@ source("load_inputs.R")
 source("farm_module.R")
 
 ## Function to simulate one production batch
-## generates 5 outputs for all production days
-## 1) prevalence     := Proportion of infected broilers 
-## 2) load           := Total ESBL E. coli in the environment (CFU)
-## 3) total_feces    := Total amount of feces in broiler's gut (g)
-## 4) concentration  := load/total_feces
-## 5) total_esbl_gut := Total ESBL E. coli in broiler's gut (CFU)
+## generates 7 outputs for all days (QoIs from Becker et al. (2022))
+## 1) load                 (CFU)   := Total ESBL E. coli in the environment till
+##                                      this day
+## 2) prevalence           (prop.) := Proportion of infected broilers 
+## 3) avg_esbl_feces       (CFU/g) := Average (over broilers) ESBL E.coli 
+##                                      concentration in daily excreted feces 
+## 4) total_feces          (g)     := Total amount of feces produced by all 
+##                                      broilers till this day
+## 5) avg_esbl_gut         (CFU)   := Average (over broilers) ESBL E. coli in the 
+##                                      the broiler's gut
+## 6) avg_esbl_env         (CFU)   := Average (over broilers) ESBL E. coli excreted
+##                                      in the environment
+## 7) avg_esbl_ingested    (CFU)   := Average (over broilers) ESBL E. coli ingested
+##                                      by a broiler
+
 batch_simulator <- function(farm_module = new.farm_module()) {
   
   # initialization
@@ -22,11 +31,13 @@ batch_simulator <- function(farm_module = new.farm_module()) {
     animals <- farm_module$run(animals, day_idx)
     
     # store daily outputs
-    output$prevalence     <- c(output$prevalence, sum(animals$B_infection_status)/nrow(animals))
-    output$load           <- c(output$load, sum(animals$C_sum_esbl_env))
-    output$total_feces    <- c(output$total_feces, sum(animals$sum_feces_gut))
-    output$concentration  <- c(output$concentration, sum(animals$C_sum_esbl_env)/sum(animals$sum_feces_gut))
-    output$total_esbl_gut <- c(output$total_esbl_gut, sum(animals$C_esbl_gut))
+    output$load              <- c(output$load, sum(animals$C_sum_esbl_env))
+    output$prevalence        <- c(output$prevalence, sum(animals$B_infection_status)/nrow(animals))  
+    output$avg_esbl_feces    <- c(output$avg_esbl_feces, mean(animals$C_esbl_env/animals$feces_gut))  
+    output$total_feces       <- c(output$total_feces, sum(animals$sum_feces_gut))
+    output$avg_esbl_gut      <- c(output$avg_esbl_gut, mean(animals$C_esbl_gut))
+    output$avg_esbl_env      <- c(output$avg_esbl_env, mean(animals$C_esbl_env))
+    output$avg_esbl_ingested <- c(output$avg_esbl_ingested, mean(animals$ingested_feces) * sum(animals$C_sum_esbl_env) / sum(animals$sum_feces_gut))
     
     # update day index
     day_idx <- day_idx + 1
--- run_farm_module_parallel.R
+++ run_farm_module_parallel.R
@@ -3,12 +3,8 @@ source("load_inputs.R")
 source("farm_module.R")
 
 ## Function to simulate production batches in parallel
-## generates a 3D array: days x 5 outputs x n_sim 
-## 1) prevalence     := Proportion of infected broilers 
-## 2) load           := Total ESBL E. coli in the environment (CFU)
-## 3) total_feces    := Total amount of feces in broiler's gut (g)
-## 4) concentration  := load/total_feces
-## 5) total_esbl_gut := Total ESBL E. coli in broiler's gut (CFU)
+## generates a 3D array: days-1 x 7 outputs x n_sim 
+
 batch_simulator_parallel <- function(farm_module = new.farm_module(), n_sim) {
   
   # custom bind function
@@ -27,7 +23,10 @@ batch_simulator_parallel <- function(farm_module = new.farm_module(), n_sim) {
   opts <- list(progress = progress)
   
   day_max <- farm_module$params$day.max
-  output <- array(0, dim = c(day_max - 1, n_sim, 5))  # Initialize a 3D array to store results
+  
+  # Initialize a 3D array to store results 
+  # TODO: remove hard coded dimension
+  output <- array(0, dim = c(day_max - 1, n_sim, 7))  
   
   output <- foreach(i = 1:n_sim, .combine = mybind, .options.snow = opts) %dopar% {
     source("run_farm_module.R")
